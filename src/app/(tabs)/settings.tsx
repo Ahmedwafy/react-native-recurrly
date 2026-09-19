8be66098e324a7541658cd931fe68f409a89e16d
Ahmed Wafy <ahmedwafyasu@gmail.com>
2026-09-19 05:02:53 +0300

    feat: project setup, nativewind, routing, tab navigation

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+// import { SafeAreaView } from "react-native"; → Deprecated in React Native 0.70, use react-native-safe-area-context instead
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context"; // React Native docs
+
+// Wrap the SafeAreaView component with the styled function to let NativeWind handle the styling
+const SafeAreaView = styled(RNSafeAreaView);
 
 const settings = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>settings</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- src/app/(tabs)/subscriptions.tsx
+++ src/app/(tabs)/subscriptions.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+// import { SafeAreaView } from "react-native"; → Deprecated in React Native 0.70, use react-native-safe-area-context instead
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context"; // React Native docs
+
+// Wrap the SafeAreaView component with the styled function to let NativeWind handle the styling
+const SafeAreaView = styled(RNSafeAreaView);
 
 const subscriptions = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text>subscriptions</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
